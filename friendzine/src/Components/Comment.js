ae3e32aeb7667b7de673820ebce56f01d0ec7772
Your Name <controlsltrapidfire@gmail.comm>
2018-02-04 16:45:27 -0600

    component and containers folders and first files

---
+++ friendzine/src/Components/Comment.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+//import './Comment.css';
+
+class Comment extends Component {
+  render() {
+    return (
+      <div className="Comment">
+        <p className="Commentary">
+          Comments goes here
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Comment;
---
+++ friendzine/src/Components/Conversation.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+//import './Conversation.css';
+
+class Conversation extends Component {
+  render() {
+    return (
+      <div className="Conversation">
+        <p className="Convo">
+          Feed goes here
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Conversation;
