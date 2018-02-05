86b78fd60dbdd4607b7e7b94f1529f28020d9280
Your Name <controlsltrapidfire@gmail.comm>
2018-02-05 16:43:47 -0600

    added example container code

--- friendzine/src/Containers/CommentContainer.js
+++ friendzine/src/Containers/CommentContainer.js
@@ -1,1 +1,13 @@
-//CommentContainer
+//some example code from:
+//https://medium.com/@learnreact/container-components-c0e67432e005
+
+class CommentListContainer extends React.Component {
+  state = { comments: [] };
+  componentDidMount() {
+    fetchSomeComments(comments =>
+      this.setState({ comments: comments }));
+  }
+  render() {
+    return <CommentList comments={this.state.comments} />;
+  }
+}
