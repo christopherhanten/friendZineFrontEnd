1dbc7d81eacf44ad652d859f8eeeadca479ca10f
Your Name <controlsltrapidfire@gmail.comm>
2018-02-06 16:04:18 -0600

    started with reactstrap

--- friendzine/src/Containers/CommentContainer.js
+++ friendzine/src/Containers/CommentContainer.js
@@ -1,5 +1,22 @@
 //some example code from:
 //https://medium.com/@learnreact/container-components-c0e67432e005
+import { FormText } from 'reactstrap';
+import { Button } from 'reactstrap';
+
+export default class Example extends React.Component {
+  render() {
+    return (
+      <FormGroup>
+          <Label for="vanillaComment">Leave a comment</Label>
+          <Input type="textarea" name="text" id="exampleText" />
+      </FormGroup>
+
+      <div>
+        <Button outline color="primary">Submit</Button>{' '}
+      </div>
+      );
+    }
+  }
 
 class CommentListContainer extends React.Component {
   state = { comments: [] };
@@ -11,3 +28,5 @@ class CommentListContainer extends React.Component {
     return <CommentList comments={this.state.comments} />;
   }
 }
+
+export default CommentContainer
