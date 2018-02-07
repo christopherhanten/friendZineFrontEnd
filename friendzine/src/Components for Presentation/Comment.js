b255e84a9e7079d843eed36672fd729b8ae4192a
Chanten <controlsltrapidfire@gmail.comm>
2018-02-07 13:44:14 -0600

    working with components

--- friendzine/src/Components for Presentation/Comment.js
+++ friendzine/src/Components for Presentation/Comment.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
+import { FormText } from 'reactstrap';
+import { Button } from 'reactstrap';
 
-//import './Comment.css';
+import './Comment.css';
 
 class Comment extends Component {
   render() {
     return (
       <div className="Comment">
-        import './CommentContainer';
+        <FormGroup>
+            <Label for="vanillaComment">Leave a comment</Label>
+            <Input type="textarea" name="text" id="exampleText" />
+        </FormGroup>
+      </div>
+      <div>
+        <Button outline color="blue">Submit</Button>{' '}
+      </div>
         <p className="Commentary">
 
         </p>
-      </div>
+
     );
   }
 }
---
+++ friendzine/src/Containers/CommentListContainer.js
@@ -0,0 +1,18 @@
+//some example code from:
+//https://medium.com/@learnreact/container-components-c0e67432e005
+
+export default class Example extends React.Component {
+  render() {
+    return (
+      class CommentListContainer extends React.Component {
+      state = { comments: [] };
+      componentDidMount() {
+        fetchSomeComments(comments =>
+          this.setState({ comments: comments }));
+      }
+  render() {
+    return <CommentList comments={this.state.comments} />;
+  }
+}
+
+export default CommentContainer
